82719ff1852a0e0b1703546d073e4049f50421db
premanan <C02864@g-able.com>
2021-12-09 22:28:14 +0700

    add code branch 2

---
+++ SpringReact/src/main/java/com/example/demo/web/ProjectController.java
@@ -0,0 +1,25 @@
+package com.example.demo.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.domain.Project;
+import com.example.demo.services.ProjectService;
+
+@RestController
+@RequestMapping("/api/project")
+public class ProjectController {
+
+	@Autowired
+	private ProjectService projectService;
+	@PostMapping("")
+	public ResponseEntity<Project> createNewProject(@RequestBody Project project){	
+		Project project1 = projectService.saveOrUpdateProject(project);
+		return new ResponseEntity<Project>(project, HttpStatus.CREATED);
+	}
+}
