cc9854cc801b5465f59ae2be3157edacd131adb3
premanan <montree_iq205178@hotmail.com>
2022-03-05 02:07:59 +0700

    add generate token key login  branch 65

--- SpringReact/src/main/java/com/example/demo/security/SecurityConfig.java
+++ SpringReact/src/main/java/com/example/demo/security/SecurityConfig.java
@@ -1,12 +1,23 @@
 package com.example.demo.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.example.demo.services.CustomUserDetailsService;
+
+import static com.example.demo.security.SecurityConstants.SIGN_UP_URLS;
+import static com.example.demo.security.SecurityConstants.H2_URL;
 
 @Configuration
 @EnableWebSecurity
@@ -20,6 +31,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private JwtAuthenticationEntryPoint unathorizedHandler;
 	
+	@Autowired
+	private CustomUserDetailsService customUserDetailsService;
+	
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(customUserDetailsService).passwordEncoder(bCryptPasswordEncoder);
+	}
+
+	@Override
+	@Bean(BeanIds.AUTHENTICATION_MANAGER)
+	protected AuthenticationManager authenticationManager() throws Exception {
+		return super.authenticationManager();
+	}
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		// Use Database
@@ -28,8 +56,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //		.sessionManagement()
 //		.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 //		.and()
-//		//.headers().frameOptions().sameOrigin() //to enable h2 database
-//		//.and()
+//		.headers().frameOptions().sameOrigin() //to enable h2 database
+//		.and()
 //		.authorizeHttpRequests()
 //		.antMatchers("/",
 //				"/favicon.ico",
@@ -41,8 +69,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //				"/**/*.html",
 //				"/**/*.css",
 //				"/**/*.js").permitAll()
-//		.antMatchers("/api/users/**").permitAll()
+//		.antMatchers(SIGN_UP_URLS).permitAll()
+//		.antMatchers(H2_URL).permitAll()
 //		.anyRequest().authenticated();
+		
+		
 		// Use H2 Console
 		 http.cors().and().csrf().disable();
 		 http.headers().frameOptions().disable();
