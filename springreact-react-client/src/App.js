3d91a48249640065f9ed1067d083f65083b73ae7
premanan <montree_iq205178@hotmail.com>
2022-03-19 05:28:14 +0700

    add code branch 77

--- springreact-react-client/src/App.js
+++ springreact-react-client/src/App.js
@@ -15,6 +15,24 @@ import UpdateProjectTask from "./components/ProjectBoard/ProjectTasks/UpdateProj
 import Landing from "./components/Layout/Landing";
 import Register from "./components/UserManagement/Register";
 import Login from "./components/UserManagement/Login";
+import jwtDecode from "jwt-decode";
+import setJWTToken from "./securityUtils/setJWTToken";
+import { SET_CURRENT_USER } from "./actions/types";
+const jwtToken = localStorage.jwtToken
+if(jwtToken){
+  setJWTToken(jwtToken);
+  const decode_jwtToken = jwtDecode(jwtToken);
+  store.dispatch({
+    type: SET_CURRENT_USER,
+    payload: decode_jwtToken
+});
+  const currentTime = Date.now() / 1000;
+  if(decode_jwtToken.exp < currentTime){
+    //handle logout
+    //window.location.href = "/";
+  }
+}
+
 class App extends Component {
   render() {
     return (
