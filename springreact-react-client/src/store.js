1fdd6ed2bbf2e26a4938c376fafa435bb5a7d653
premanan <montree_iq205178@hotmail.com>
2022-04-07 01:05:08 +0700

    edit title name

--- springreact-react-client/src/store.js
+++ springreact-react-client/src/store.js
@@ -16,11 +16,6 @@ if (window.navigator.userAgent.includes("Chrome") && ReactReduxDevTools) {
     compose(
       applyMiddleware(...middleware),
       ReactReduxDevTools
-
-      // typeof window.__REDUX_DEVTOOLS_EXTENSION__ === "undefined"
-      // ? a => a
-      // : window.__REDUX_DEVTOOLS_EXTENSION__ &&
-      //     window.__REDUX_DEVTOOLS_EXTENSION__()
     )
   );
 } else {
